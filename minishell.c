4a398c8725f48c98c1bef96589bcaa77c3ee219d
adrian <gz.adrian.200@gmail.com>
2024-11-13 09:47:16 +0100

    input redirection enabled

--- minishell.c
+++ minishell.c
@@ -35,14 +35,24 @@ void main(){
                 //Si el archivo NO existe se crea y se abre. Si el archivo SI existe se abre
                 FILE *file = fopen(line->redirect_output, "w");
                 //Comprobar si ha fallado fopen
-                if(file == NULL) fprintf(stderr, "No se pudo abrir el archivo %s", line->redirect_output);
+                if(file == NULL) fprintf(stderr, "No se pudo abrir el archivo %s\n", line->redirect_output);
                 int fd_file = fileno(file);
                 dup2(fd_file, STDOUT_FILENO);
                 fclose(file);
             }
 
+            if(line->redirect_input != NULL) {
+                FILE *file = fopen(line->redirect_input, "r");
+                //Si el archivo de entrada no existe hay que mostrar un error
+                if(file == NULL) fprintf(stderr, "No se pudo abrir el archivo de entrada %s\n", line->redirect_input);
+                int fd_file = fileno(file);  //Se coge el fd del archivo
+                dup2(fd_file, STDIN_FILENO);    //Se sustituye el fd de salida estandar por el fd del fichero para que la salida se guarde ahí
+                fclose(file);
+
+            }
+
             execvp(line->commands[0].filename, line->commands[0].argv);
-            fprintf(stderr, "Ha habido un problema ejecutando %s", line->commands[0].filename);
+            fprintf(stderr, "Ha habido un problema ejecutando %s\n", line->commands[0].filename);
             exit(1);
         }
         //padre 
